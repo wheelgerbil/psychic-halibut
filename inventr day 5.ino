c8f2fd1fe39e51c20781051fc613085f502870cb
wheelgerbil <121492215+wheelgerbil@users.noreply.github.com>
2023-01-09 18:32:59 +0000

    Add files via upload

---
+++ inventr day 5.ino
@@ -0,0 +1,63 @@
+int LEDwhite = 8;
+int LEDblue = 9;
+int LEDyellow = 10;
+int LEDgreen = 11;
+int LEDred = 12;
+
+
+int Switch1 = 2;
+int Switch2 = 3;
+int Switch3 = 4;
+
+void setup() {
+  pinMode(LEDwhite, OUTPUT);
+  pinMode(LEDblue, OUTPUT);
+  pinMode(LEDyellow, OUTPUT);
+  pinMode(LEDgreen, OUTPUT);
+  pinMode(LEDred, OUTPUT);
+
+  pinMode(Switch1, INPUT);
+  pinMode(Switch2, INPUT);
+  pinMode(Switch3, INPUT);
+}
+
+void loop(){
+
+  if (digitalRead(Switch1 || Switch2 || Switch3) == HIGH){
+  digitalWrite(LEDgreen, HIGH);
+
+}
+else{
+  digitalWrite(LEDgreen, LOW);
+  digitalWrite(LEDred, HIGH);
+}
+
+
+if (digitalRead(Switch1)==HIGH){
+  digitalWrite(LEDwhite, HIGH);
+  delay(100);
+  digitalWrite(LEDwhite, LOW);
+}
+else{
+  (LEDwhite, LOW);
+  }
+
+if (digitalRead(Switch2)==HIGH){
+  digitalWrite(LEDblue, HIGH);
+  delay(100);
+  digitalWrite(LEDblue, LOW);
+}
+else{(LEDblue, LOW);}
+
+if (digitalRead(Switch3)==HIGH){
+  digitalWrite(LEDyellow, HIGH);
+  delay(100);
+  digitalWrite(LEDyellow, LOW);
+}
+else{
+(LEDyellow, LOW);  
+}
+
+
+
+}
